42da86c3a0a35891a2eabafbfd3d972ef5e7e1f7
Joe Linstrum <jlinstrum@NEW-INNOV.COM>
2016-04-28 16:30:09 -0400

    Fee

---
+++ application/views/home/WelcomeNonMember_pc.php
@@ -0,0 +1,12 @@
+<?php
+	echo $head_start;
+	echo $body_start;	
+	$site = SITE;
+?>
+
+<div class='container-wide-white'>
+	<div id='nav-tier1' class='container-center'>
+		<a href='<?php echo $site;?>'><img src="<?php echo $site;?>/_images/site/favicon.png">
+	</div>
+
+</div>
