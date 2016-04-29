d0923980a95f99d8953878529f979bccadd2df91
joe44850 <joelinstrum@yahoo.com>
2016-04-29 07:03:50 -0400

    nothing

--- application/views/home/WelcomeNonMember_pc.php
+++ application/views/home/WelcomeNonMember_pc.php
@@ -6,7 +6,10 @@
 
 <div class='container-wide-white'>
 	<div id='nav-tier1' class='container-center'>
-		<a href='<?php echo $site;?>'><img src="<?php echo $site;?>/_images/site/favicon.png">
+		<a href='<?php echo $site;?>'><img src="<?php echo $site;?>/_images/site/favicon.png"></a>
+		<a href='<?php echo $site;?>'>Questions</a>
+		<a href='<?php echo $site;?>'>Stories</a>
+		<a href='<?php echo $site;?>'>Groups</a>
 	</div>
 
 </div>
